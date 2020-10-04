9322001aec9fe8e5b07a3bd9d38a8983973071cf
Janani NA <janani2285@gmail.com>
2020-10-04 00:20:52 -0700

    list of courses

--- src/main/java/io/springbootstarter/topic/TopicController.java
+++ src/main/java/io/springbootstarter/topic/TopicController.java
@@ -1,5 +1,8 @@
 package io.springbootstarter.topic;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class TopicController {
 
 	@RequestMapping("/topics")
-	public String getTopics() {
-		return "All Topics";
+	public List<Topic> getTopics() {
+		return Arrays.asList(
+				new Topic("Spring", "Spring Framework", "Spring Description"),
+				new Topic("Java", "Java", "Java Description"),
+				new Topic("JavaScript", "JavaScript", "JavaScript Description")
+				);
 	}
 }
